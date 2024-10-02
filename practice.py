2cd0f4b0e09ea660c40a7bd907da875802c8c8dc
Rock13hound <rock13hound@gmail.com>
2024-10-02 01:37:04 +0000

    done

---
+++ practice.py
@@ -0,0 +1,13 @@
+items = ["milk","Bananas","eggs","Granola","Avocado"]
+prices = [2, 1, 3.5, 2.5, .58]
+
+for index in range(len(items)):
+    print(f"I bought {items[index]} for ${prices[index]}")
+
+total = 0
+for price in prices:
+    total = price + total
+print(f"I paid ${total} for everything")
+
+#max(prices)
+#print({index})
